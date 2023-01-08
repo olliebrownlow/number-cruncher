c42093b17d7656e644f929fad04fd1ec3a11cc79
Oliver Brownlow <olliebrownlow@gmail.com>
2023-01-08 23:07:36 +0000

    adds array of game links to map over on home screen

---
+++ config/gameLinks.js
@@ -0,0 +1,40 @@
+const gameLinks = [
+  {
+    pathName: "/game-options",
+    gameType: "practice-mode",
+    gameTitle: "Practice mode",
+    gameDescription:
+      "Select times tables of your choice and get those brain cells  working!",
+  },
+  {
+    pathName: "/game-options",
+    gameType: "streak",
+    gameTitle: "Streak",
+    gameDescription: "Get one wrong and the streak counter will reset to zero!",
+  },
+  {
+    pathName: "/game-options",
+    gameType: "play-the-percentages",
+    gameTitle: "Play the Percentages",
+    gameDescription: "Set a question limit and see how many you can get right.",
+  },
+  {
+    pathName: "/game-options",
+    gameType: "against-the-clock",
+    gameTitle: "Against the Clock",
+    gameDescription:
+      "Set time limits and compete for top scores. Tick tock tick tock...",
+  },
+  {
+    pathName: "/journey",
+    gameTitle: "Journey",
+    gameDescription: "Work through levels of increasing difficulty earning rewards as you go.",
+  },
+  {
+    pathName: "/about",
+    gameTitle: "About",
+    gameDescription: "What, who and why..",
+  },
+];
+
+export default gameLinks;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import PageHeading from "../components/pageHeading";
+import gameLinks from "../config/gameLinks";
 import GameLink from "../components/gameLink";
 import styles from "../styles/Home.module.css";
 
@@ -8,55 +9,14 @@ const Home = (props) => {
       <PageHeading heading={"Number Cruncher"} />
 
       <div className={styles.grid}>
-        <GameLink
-          pathName={"/game-options"}
-          gameType={"practice-mode"}
-          gameTitle={"Practice mode"}
-          gameDescription={
-            "Select times tables of your choice and get those brain cells  working!"
-          }
-        />
-
-        <GameLink
-          pathName={"/game-options"}
-          gameType={"streak"}
-          gameTitle={"Streak"}
-          gameDescription={
-            "Get one wrong and the streak counter will reset to zero!"
-          }
-        />
-
-        <GameLink
-          pathName={"/game-options"}
-          gameType={"play-the-percentages"}
-          gameTitle={"Play the percentages"}
-          gameDescription={
-            "Set a question limit and see how many you can get right."
-          }
-        />
-
-        <GameLink
-          pathName={"/game-options"}
-          gameType={"against-the-clock"}
-          gameTitle={"Against the clock"}
-          gameDescription={
-            "Set time limits and compete for top scores. Tick tock tick tock..."
-          }
-        />
-
-        <GameLink
-          pathName={"/journey"}
-          gameTitle={"Journey"}
-          gameDescription={
-            "Work through levels of increasing difficulty earning rewards as you go."
-          }
-        />
-
-        <GameLink
-          pathName={"/about"}
-          gameTitle={"About"}
-          gameDescription={"What, who and why.."}
-        />
+        {gameLinks.map((link) => (
+          <GameLink
+            pathName={link.pathName}
+            gameType={link.gameType}
+            gameTitle={link.gameTitle}
+            gameDescription={link.gameDescription}
+          />
+        ))}
       </div>
     </>
   );
