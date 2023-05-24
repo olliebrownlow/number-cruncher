dc9480d04dc57f332a9f6d19c3520be2c4715a7f
Oliver Brownlow <olliebrownlow@gmail.com>
2023-05-24 17:43:18 +0000

    adds release info for release 6

--- config/releaseInfo.js
+++ config/releaseInfo.js
@@ -5,32 +5,35 @@ const releaseInfo = [
     features: "New feature(s)",
   },
   {
+    release: "6",
+    date: "May 24 2023",
+    features: "Introduces gem collection, the machine and the first challenge",
+  },
+  {
     release: "5",
-    date: "Apr 20th 2023",
+    date: "Apr 20 2023",
     features:
       "New repeat user achievement for use of number cruncher on consecutive days",
   },
   {
     release: "4",
-    date: "Apr 7th 2023",
-    features:
-      "Streak challenge and 3 related achievement goals",
+    date: "Apr 7 2023",
+    features: "Streak challenge and 3 related achievement goals",
   },
   {
     release: "3",
-    date: "Mar 4th 2023",
-    features:
-      "Progress history tracking to monitor user progress over time",
+    date: "Mar 4 2023",
+    features: "Progress history tracking to monitor user progress over time",
   },
   {
     release: "2",
-    date: "Feb 16th 2023",
+    date: "Feb 16 2023",
     features:
       "First achievement goals functionality for overall correct answers",
   },
   {
     release: "1",
-    date: "Feb 8th 2023",
+    date: "Feb 8 2023",
     features: "Practice mode functionality",
   },
 ];
